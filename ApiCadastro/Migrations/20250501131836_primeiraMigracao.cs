c484a395885a1ba5d83bb22b7b9e3a6c1ee716c1
Luan Vinícius <luanviniciuzz@gmail.com>
2025-05-01 10:52:27 -0300

    primeiro commit

---
+++ ApiCadastro/Migrations/20250501131836_primeiraMigracao.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCadastro.Migrations
+{
+    /// <inheritdoc />
+    public partial class primeiraMigracao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
