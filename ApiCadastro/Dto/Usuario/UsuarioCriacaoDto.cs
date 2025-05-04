87ed155f5524d164cb069928961ae2fe962e6887
Luan Vinícius <luanviniciuzz@gmail.com>
2025-05-04 20:30:43 -0300

    feat: dto de usuario criacao e edicao

---
+++ ApiCadastro/Dto/Usuario/UsuarioCriacaoDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCadastro.Dto.Usuario
+{
+    public class UsuarioCriacaoDto
+    {
+        [Required(ErrorMessage = "Digite o Usuário")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Digite o Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Sobrenome")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Email")]
+        public string Email { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+
+        [Required(ErrorMessage = "Digite o Senha")]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Digite a Confirmação da Senha"), Compare("Senha", ErrorMessage = "As senhas não são iguais")]
+        public string ConfirmaSenha { get; set; }
+    }
+}
---
+++ ApiCadastro/Dto/Usuario/UsuarioEdicaoDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCadastro.Dto.Usuario
+{
+    public class UsuarioEdicaoDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o Usuário")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Digite o Nome")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Sobrenome")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Digite o Email")]
+        public string Email { get; set; }
+
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAlteracao { get; set; }
+
+    }
+}
